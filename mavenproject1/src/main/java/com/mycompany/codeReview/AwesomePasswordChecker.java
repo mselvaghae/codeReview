8214f21d234845b642cefb6e5f71d605b7936166
mselvaghae <mohamed_salem.Elvagha.Etaher@etu.uca.fr>
2024-12-18 01:08:29 +0100

    fix bugs of checkstyle and test

--- mavenproject1/src/main/java/com/mycompany/codeReview/AwesomePasswordChecker.java
+++ mavenproject1/src/main/java/com/mycompany/codeReview/AwesomePasswordChecker.java
@@ -13,255 +13,290 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Utility class for analyzing and checking password strength using clustering.
- * This class ensures only one instance is used throughout the application.
+ * Utility class for analyzing and checking password strength using clustering. This class ensures
+ * only one instance is used throughout the application.
  */
 public final class AwesomePasswordChecker {
 
-    // Constants to replace magic numbers
-    private static final int MASK_ARRAY_SIZE = 28;
-    private static final int LOWERCASE_MASK = 2;
-    private static final int UPPERCASE_MASK = 4;
-    private static final int DIGIT_MASK = 5;
-    private static final int SPECIAL_CHAR_MASK = 6;
-    private static final int OTHER_MASK = 7;
+  // Singleton instance
+  private static AwesomePasswordChecker instance;
 
-    // Singleton instance
-    private static AwesomePasswordChecker instance;
+  // List of cluster centers for distance computation
+  private final List<double[]> clusterCenters = new ArrayList<>();
 
-    // List of cluster centers for distance computation
-    private final List<double[]> clusterCenters = new ArrayList<>();
-
-    /**
-     * Retrieves the singleton instance, initialized with the specified cluster center file.
-     *
-     * @param file The file containing cluster center data.
-     * @return The singleton instance of {@link AwesomePasswordChecker}.
-     * @throws IOException If an error occurs while reading the file.
-     */
-    public static AwesomePasswordChecker getInstance(final File file) throws IOException {
-        if (instance == null) {
-            instance = new AwesomePasswordChecker(new FileInputStream(file));
-        }
-        return instance;
+  /**
+   * Retrieves the singleton instance, initialized with the specified cluster center file.
+   *
+   * @param file The file containing cluster center data.
+   * @return The singleton instance of {@link AwesomePasswordChecker}.
+   * @throws IOException If an error occurs while reading the file.
+   */
+  public static AwesomePasswordChecker getInstance(final File file) throws IOException {
+    if (instance == null) {
+      instance = new AwesomePasswordChecker(new FileInputStream(file));
     }
+    return instance;
+  }
 
-    /**
-     * Retrieves the singleton instance, initialized with the default resource file.
-     *
-     * @return The singleton instance of {@link AwesomePasswordChecker}.
-     * @throws IOException If an error occurs while reading the default resource file.
-     */
-    public static AwesomePasswordChecker getInstance() throws IOException {
-        if (instance == null) {
-            InputStream is = AwesomePasswordChecker.class.getClassLoader()
-                    .getResourceAsStream("cluster_centers_HAC_aff.csv");
-            instance = new AwesomePasswordChecker(is);
-        }
-        return instance;
+  /**
+   * Retrieves the singleton instance, initialized with the default resource file.
+   *
+   * @return The singleton instance of {@link AwesomePasswordChecker}.
+   * @throws IOException If an error occurs while reading the default resource file.
+   */
+  public static AwesomePasswordChecker getInstance() throws IOException {
+    if (instance == null) {
+      InputStream is =
+          AwesomePasswordChecker.class
+              .getClassLoader()
+              .getResourceAsStream("cluster_centers_HAC_aff.csv");
+      instance = new AwesomePasswordChecker(is);
     }
+    return instance;
+  }
 
-    /**
-     * Private constructor initializes the checker with cluster center data.
-     *
-     * @param is The input stream containing cluster center data.
-     * @throws IOException If an error occurs while reading the input stream.
-     */
-    private AwesomePasswordChecker(final InputStream is) throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(";");
-                double[] center = new double[values.length];
-                for (int i = 0; i < values.length; ++i) {
-                    try {
-                        center[i] = Double.parseDouble(values[i].replace(",", ".")); // Remplacer les virgules
-                    } catch (NumberFormatException e) {
-                        throw new IOException("Erreur de format dans le fichier CSV : " + values[i], e);
-                    }
-                }
-                clusterCenters.add(center);
-            }
+  /**
+   * Private constructor initializes the checker with cluster center data.
+   *
+   * @param is The input stream containing cluster center data.
+   * @throws IOException If an error occurs while reading the input stream.
+   */
+  private AwesomePasswordChecker(final InputStream is) throws IOException {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+      String line;
+      while ((line = br.readLine()) != null) {
+        String[] values = line.split(",");
+        double[] center = new double[values.length];
+        for (int i = 0; i < values.length; ++i) {
+          try {
+            center[i] = Double.parseDouble(values[i].replace(",", "."));
+          } catch (NumberFormatException e) {
+            throw new IOException("Erreur de format dans le fichier CSV : " + values[i], e);
+          }
         }
+        clusterCenters.add(center);
+      }
     }
+  }
 
+  /**
+   * Generates a numerical mask for a given password based on character rules.
+   *
+   * @param password The password to analyze.
+   * @return An integer array representing the character mask of the password.
+   */
+  public int[] maskAff(String password) {
+    int limit = Math.min(password.length(), 28);
+    int[] maskArray = new int[limit];
 
-
-    /**
-     * Generates a numerical mask for a given password based on character rules.
-     *
-     * @param password The password to analyze.
-     * @return An integer array representing the character mask of the password.
-     */
-    public int[] maskAff(final String password) {
-        int[] maskArray = new int[MASK_ARRAY_SIZE];
-        int limit = Math.min(password.length(), MASK_ARRAY_SIZE);
-
-        for (int i = 0; i < limit; ++i) {
-            char c = password.charAt(i);
-            if (Character.isLowerCase(c)) {
-                maskArray[i] = LOWERCASE_MASK;
-            } else if (Character.isUpperCase(c)) {
-                maskArray[i] = UPPERCASE_MASK;
-            } else if (Character.isDigit(c)) {
-                maskArray[i] = DIGIT_MASK;
-            } else if ("<>-?.!/".contains(String.valueOf(c))) {
-                maskArray[i] = SPECIAL_CHAR_MASK;
-            } else {
-                maskArray[i] = OTHER_MASK;
-            }
-        }
-        return maskArray;
+    for (int i = 0; i < limit; ++i) {
+      char c = password.charAt(i);
+      switch (c) {
+        case 'e':
+        case 's':
+        case 'a':
+        case 'i':
+        case 't':
+        case 'n':
+        case 'r':
+        case 'u':
+        case 'o':
+        case 'l':
+          maskArray[i] = 1;
+          break;
+        case 'E':
+        case 'S':
+        case 'A':
+        case 'I':
+        case 'T':
+        case 'N':
+        case 'R':
+        case 'U':
+        case 'O':
+        case 'L':
+          maskArray[i] = 3;
+          break;
+        case '>':
+        case '<':
+        case '-':
+        case '?':
+        case '.':
+        case '/':
+        case '!':
+        case '%':
+        case '@':
+        case '&':
+          maskArray[i] = 6;
+          break;
+        default:
+          if (Character.isLowerCase(c)) {
+            maskArray[i] = 2;
+          } else if (Character.isUpperCase(c)) {
+            maskArray[i] = 4;
+          } else if (Character.isDigit(c)) {
+            maskArray[i] = 5;
+          } else {
+            maskArray[i] = 7;
+          }
+      }
     }
+    return maskArray;
+  }
 
-    /**
-     * Computes the minimum Euclidean distance between the password's mask and cluster centers.
-     *
-     * @param password The password to analyze.
-     * @return The minimum distance as a {@code double}.
-     */
-    public double getDistance(final String password) {
-        int[] maskArray = maskAff(password);
-        double minDistance = Double.MAX_VALUE;
-        for (double[] center : clusterCenters) {
-            minDistance = Math.min(euclideanDistance(maskArray, center), minDistance);
-        }
-        return minDistance;
+  /**
+   * Computes the minimum Euclidean distance between the password's mask and cluster centers.
+   *
+   * @param password The password to analyze.
+   * @return The minimum distance as a {@code double}.
+   */
+  public double getDistance(final String password) {
+    int[] maskArray = maskAff(password);
+    double minDistance = Double.MAX_VALUE;
+
+    for (double[] center : clusterCenters) {
+      minDistance = Math.min(euclideanDistance(maskArray, center), minDistance);
     }
+    return minDistance;
+  }
 
-    /**
-     * Computes the Euclidean distance between two vectors.
-     *
-     * @param a The first vector (integer array).
-     * @param b The second vector (double array).
-     * @return The Euclidean distance as a {@code double}.
-     */
-    private double euclideanDistance(final int[] a, final double[] b) {
-        double sum = 0;
-        for (int i = 0; i < a.length; i++) {
-            sum += (a[i] - b[i]) * (a[i] - b[i]);
-        }
-        return Math.sqrt(sum);
+  /**
+   * Computes the Euclidean distance between two vectors.
+   *
+   * @param a The first vector (integer array).
+   * @param b The second vector (double array).
+   * @return The Euclidean distance as a {@code double}.
+   */
+  private double euclideanDistance(final int[] a, final double[] b) {
+    double sum = 0;
+    for (int i = 0; i < a.length; i++) {
+      sum += (a[i] - b[i]) * (a[i] - b[i]);
     }
+    return Math.sqrt(sum);
+  }
 
-    /**
- * Computes the MD5 hash of the input string using a custom implementation.
- *
- * @param input the string to hash
- * @return the MD5 hash as a hexadecimal string
- */
-public static String computeMd5(final String input) {
+  /**
+   * Computes the MD5 hash of the input string using a custom implementation.
+   *
+   * @param input the string to hash
+   * @return the MD5 hash as a hexadecimal string
+   */
+  public static String computeMd5(final String input) {
     try {
-        // Step 1: Convert input string to bytes
-        final byte[] message = input.getBytes("UTF-8");
-        final int messageLenBytes = message.length;
+      final byte[] message = input.getBytes("UTF-8");
+      final int messageLenBytes = message.length;
+      final int numBlocks = ((messageLenBytes + 8) >>> 6) + 1;
+      final int totalLen = numBlocks << 6;
+      final byte[] paddingBytes = new byte[totalLen - messageLenBytes];
+      paddingBytes[0] = (byte) 0x80;
 
-        // Step 2: Compute number of blocks and total length
-        final int numBlocks = ((messageLenBytes + 8) >>> 6) + 1;
-        final int totalLen = numBlocks << 6;
-        final byte[] paddingBytes = new byte[totalLen - messageLenBytes];
-        paddingBytes[0] = (byte) 0x80;
+      final long messageLenBits = (long) messageLenBytes << 3;
+      final ByteBuffer lengthBuffer =
+          ByteBuffer.allocate(8).order(ByteOrder.LITTLE_ENDIAN)
+              .putLong(messageLenBits);
+      final byte[] lengthBytes = lengthBuffer.array();
 
-        // Step 3: Append length in bits as a 64-bit little-endian integer
-        final long messageLenBits = (long) messageLenBytes << 3;
-        final ByteBuffer lengthBuffer = ByteBuffer.allocate(8)
-                .order(ByteOrder.LITTLE_ENDIAN)
-                .putLong(messageLenBits);
-        final byte[] lengthBytes = lengthBuffer.array();
+      final byte[] paddedMessage = new byte[totalLen];
+      System.arraycopy(message, 0, paddedMessage, 0, messageLenBytes);
+      System.arraycopy(paddingBytes, 0, paddedMessage, messageLenBytes, 
+          paddingBytes.length);
+      System.arraycopy(lengthBytes, 0, paddedMessage, totalLen - 8, 8);
 
-        // Step 4: Create padded message
-        final byte[] paddedMessage = new byte[totalLen];
-        System.arraycopy(message, 0, paddedMessage, 0, messageLenBytes);
-        System.arraycopy(paddingBytes, 0, paddedMessage, messageLenBytes, paddingBytes.length);
-        System.arraycopy(lengthBytes, 0, paddedMessage, totalLen - 8, 8);
+      final int[] h = {0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476};
 
-        // Step 5: Initialize MD5 hash values
-        final int[] h = {0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476};
+      // Step 6: Initialize constants
+      final int[] k = {
+        0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 
+        0xa8304613, 0xfd469501,
+        0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 
+        0xa679438e,0x49b40821,
+        0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 
+        0xd8a1e681,0xe7d3fbc8,
+        0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9,
+            0x8d2a4c8a,
+        0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60,
+            0xbebfbc70,
+        0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8,
+            0xc4ac5665,
+        0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d,
+            0x85845dd1,
+        0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb,
+            0xeb86d391
+      };
 
-        // Step 6: Initialize constants
-        final int[] k = {
-            0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
-            0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
-            0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
-            0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
-            0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
-            0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
-            0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
-            0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
-        };
+      final int[] r = {
+        7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+        5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20,
+        4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+        6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
+      };
 
-        final int[] r = {
-            7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
-            5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20,
-            4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
-            6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
-        };
-
-        // Step 7: Process message in 512-bit chunks
-        for (int i = 0; i < numBlocks; i++) {
-            final int[] w = new int[16];
-            for (int j = 0; j < 16; j++) {
-                w[j] = ByteBuffer.wrap(paddedMessage, (i << 6) + (j << 2), 4)
-                        .order(ByteOrder.LITTLE_ENDIAN).getInt();
-            }
-
-            int a = h[0];
-            int b = h[1];
-            int c = h[2];
-            int d = h[3];
-
-            for (int j = 0; j < 64; j++) {
-                final int f;
-                final int g;
-                if (j < 16) {
-                    f = (b & c) | (~b & d);
-                    g = j;
-                } else if (j < 32) {
-                    f = (d & b) | (~d & c);
-                    g = (5 * j + 1) % 16;
-                } else if (j < 48) {
-                    f = b ^ c ^ d;
-                    g = (3 * j + 5) % 16;
-                } else {
-                    f = c ^ (b | ~d);
-                    g = (7 * j) % 16;
-                }
-                final int temp = d;
-                d = c;
-                c = b;
-                b = b + Integer.rotateLeft(a + f + k[j] + w[g], r[j]);
-                a = temp;
-            }
-
-            h[0] += a;
-            h[1] += b;
-            h[2] += c;
-            h[3] += d;
+      // Step 7: Process message in 512-bit chunks
+      for (int i = 0; i < numBlocks; i++) {
+        final int[] w = new int[16];
+        for (int j = 0; j < 16; j++) {
+          w[j] =
+              ByteBuffer.wrap(paddedMessage, (i << 6) + (j << 2), 4)
+                  .order(ByteOrder.LITTLE_ENDIAN)
+                  .getInt();
         }
 
-        // Step 8: Create the output hash
-        final ByteBuffer md5Buffer = ByteBuffer.allocate(16).order(ByteOrder.LITTLE_ENDIAN);
-        md5Buffer.putInt(h[0]).putInt(h[1]).putInt(h[2]).putInt(h[3]);
-        final byte[] md5Bytes = md5Buffer.array();
+        int a = h[0];
+        int b = h[1];
+        int c = h[2];
+        int d = h[3];
 
-        final StringBuilder md5Hex = new StringBuilder();
-        for (byte b : md5Bytes) {
-            md5Hex.append(String.format("%02x", b));
+        for (int j = 0; j < 64; j++) {
+          final int f;
+          final int g;
+          if (j < 16) {
+            f = (b & c) | (~b & d);
+            g = j;
+          } else if (j < 32) {
+            f = (d & b) | (~d & c);
+            g = (5 * j + 1) % 16;
+          } else if (j < 48) {
+            f = b ^ c ^ d;
+            g = (3 * j + 5) % 16;
+          } else {
+            f = c ^ (b | ~d);
+            g = (7 * j) % 16;
+          }
+          final int temp = d;
+          d = c;
+          c = b;
+          b = b + Integer.rotateLeft(a + f + k[j] + w[g], r[j]);
+          a = temp;
         }
 
-        return md5Hex.toString();
-    }catch (UnsupportedEncodingException e) {
-        throw new RuntimeException("UTF-8 not supported", e);
-    }
-}
+        h[0] += a;
+        h[1] += b;
+        h[2] += c;
+        h[3] += d;
+      }
 
+      // Step 8: Create the output hash
+      final ByteBuffer md5Buffer = ByteBuffer.allocate(16).order(ByteOrder.LITTLE_ENDIAN);
+      md5Buffer.putInt(h[0]).putInt(h[1]).putInt(h[2]).putInt(h[3]);
+      final byte[] md5Bytes = md5Buffer.array();
 
-    /**
-     * Main method for testing the functionality of {@link AwesomePasswordChecker}.
-     *
-     * @param args Command-line arguments.
-     */
-    public static void main(final String[] args) {
-        // Example usage or testing logic here
+      final StringBuilder md5Hex = new StringBuilder();
+      for (byte b : md5Bytes) {
+        md5Hex.append(String.format("%02x", b));
+      }
+
+      return md5Hex.toString();
+
+    } catch (UnsupportedEncodingException e) {
+      throw new RuntimeException("UTF-8 not supported", e);
     }
+  }
+
+  /**
+   * Main method for testing the functionality of {@link AwesomePasswordChecker}.
+   *
+   * @param args Command-line arguments.
+   */
+  public static void main(final String[] args) {
+    // Example usage or testing logic here
+  }
 }
--- mavenproject1/src/test/java/com/mycompany/codereview/AwesomePasswordCheckerTest.java
+++ mavenproject1/src/test/java/com/mycompany/codereview/AwesomePasswordCheckerTest.java
@@ -1,118 +1,121 @@
 package com.mycompany.codereview;
 
-import com.mycompany.codeReview.AwesomePasswordChecker;
-/*import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;*/
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.mycompany.codeReview.AwesomePasswordChecker;
 import java.io.File;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-/*import java.io.IOException;*/
+import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
-/**
- * Test class for AwesomePasswordChecker.
- */
+/** Test class for AwesomePasswordChecker. */
 public class AwesomePasswordCheckerTest {
 
-    /**
-     * Test of getInstance method with file parameter.
-     *
-     * @throws Exception if an error occurs during test execution
-     */
-    @Test
-    public void testGetInstance_File() throws Exception {
-        System.out.println("getInstance");
+  /**
+   * Test of getInstance method with file parameter.
+   *
+   * @throws Exception if an error occurs during test execution
+   */
+  @Test
+  public void testGetInstance_File() throws Exception {
+    System.out.println("getInstance");
 
-        File file = new File("src/test/resources/cluster_centers_HAC_aff.csv");
-        assertTrue(file.exists(), "Le fichier de centres de clusters doit exister pour le test.");
+    File file = new File("src/test/resources/cluster_centers_HAC_aff.csv");
+    assertTrue(file.exists(), "Le fichier de centres de clusters doit exister pour le test.");
 
-        AwesomePasswordChecker result = AwesomePasswordChecker.getInstance(file);
-        assertNotNull(result, "L'instance obtenue ne doit pas être null.");
+    AwesomePasswordChecker result = AwesomePasswordChecker.getInstance(file);
+    assertNotNull(result, "L'instance obtenue ne doit pas être null.");
 
-        AwesomePasswordChecker secondResult = AwesomePasswordChecker.getInstance(file);
-        assertSame(result, secondResult, "La méthode getInstance doit retourner la même instance.");
-    }
+    AwesomePasswordChecker secondResult = AwesomePasswordChecker.getInstance(file);
+    assertSame(result, secondResult, "La méthode getInstance doit retourner la même instance.");
+  }
 
-    /**
-     * Test of getInstance method without arguments.
-     *
-     * @throws Exception if an error occurs during test execution
-     */
-    /*@Test
-    public void testGetInstance_0args() throws Exception {
-        System.out.println("getInstance with no arguments");
+  /**
+   * Test of getInstance method without arguments.
+   *
+   * @throws Exception if an error occurs during test execution
+   */
+  @Test
+  public void testGetInstance_0args() throws Exception {
+    System.out.println("getInstance with no arguments");
 
-        AwesomePasswordChecker result = AwesomePasswordChecker.getInstance();
-        assertNotNull(result, "L'instance obtenue ne doit pas être null.");
+    AwesomePasswordChecker result = AwesomePasswordChecker.getInstance();
+    assertNotNull(result, "L'instance obtenue ne doit pas être null.");
 
-        AwesomePasswordChecker secondResult = AwesomePasswordChecker.getInstance();
-        assertSame(result, secondResult, "La méthode getInstance doit retourner la même instance.");
-    }
+    AwesomePasswordChecker secondResult = AwesomePasswordChecker.getInstance();
+    assertSame(result, secondResult, "La méthode getInstance doit retourner la même instance.");
+  }
 
-    /**
-     * Test of maskAff method.
-     *
-     * @throws IOException if an error occurs during initialization
-     */
-    /*
-    @Test
-    public void testMaskAff() throws IOException {
-        System.out.println("maskAff");
+  /**
+   * Test of maskAff method.
+   *
+   * @throws IOException if an error occurs during initialization
+   */
+  @Test
+  public void testMaskAff() throws IOException {
+    System.out.println("maskAff");
 
-        String password = "Password123!";
-        AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance();
+    String password = "Password123!";
+    AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance();
 
-        int[] expectedMask = {4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 6, 6};
-        int[] result = instance.maskAff(password);
+    int[] expectedMask = {4, 1, 1, 1, 2, 1, 1, 2, 5, 5, 5, 6};
+    int[] result = instance.maskAff(password);
 
-        assertArrayEquals(expectedMask, result, "Le masque généré doit correspondre au masque attendu.");
-    }*/
+    assertArrayEquals(expectedMask, result, "Le masque généré doit correspondre au masque attendu.");
+  }
 
-    /**
-     * Test of getDistance method.
-     *
-     * @throws Exception if an error occurs during initialization
-     */
-    /*
-    @Test
-    public void testGetDistance() throws Exception {
-        System.out.println("getDistance");
+  /**
+   * Test of getDistance method.
+   *
+   * @throws Exception if an error occurs during initialization
+   */
+  @Test
+  public void testGetDistance() throws Exception {
+    System.out.println("Running test for getDistance");
 
-        String password = "Password123!";
-        AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance();
+    // Input password
+    String password = "Password123!";
 
-        double result = instance.getDistance(password);
-        assertTrue(result > 0, "La distance calculée doit être supérieure à zéro.");
-    }*/
+    // Get an instance of AwesomePasswordChecker
+    AwesomePasswordChecker instance = AwesomePasswordChecker.getInstance();
 
-    /**
-     * Test of computeMd5 method.
-     */
-    /*  @Test
-    public void testComputeMd5() {
+    // Calculate the distance
+    double result = instance.getDistance(password);
+
+    // Debugging log for distance
+    System.out.println("Calculated distance: " + result);
+
+    // Validate the result
+    assertTrue(result > 0, "La distance calculée doit être supérieure à zéro.");
+  }
+
+  /** Test of computeMd5 method. */
+  @Test
+  public void testComputeMd5() {
     System.out.println("computeMd5");
-    
+
     // Entrée et résultat attendu
     String input = "Password123!";
-    String expectedMd5 = "42f749ade7f9e195bf475f37a44cafcb";
-    
+    String expectedMd5 = "2c103f2c4ed1e59c0b4e2e01821770fa";
+
     String result = AwesomePasswordChecker.computeMd5(input);
     assertEquals(expectedMd5, result, "Le hash MD5 calculé doit correspondre au résultat attendu.");
-    }*/
+  }
+
+  /** Test of main method. */
+  @Test
+  public void testMain() {
+    System.out.println("main");
+
+    String[] args = {};
+    assertDoesNotThrow(
+        () -> AwesomePasswordChecker.main(args), "La méthode main doit s'exécuter sans erreur.");
+  }
 
 
-    /**
-     * Test of main method.
-     */
-    @Test
-    public void testMain() {
-        System.out.println("main");
 
-        String[] args = {};
-        assertDoesNotThrow(() -> AwesomePasswordChecker.main(args),
-                "La méthode main doit s'exécuter sans erreur.");
-    }
 }
